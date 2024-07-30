13a3f6b655f378b9129fc7e92e60e5dba4bcedef
Thomas <thomas.kiley@ndm.ox.ac.uk>
2024-07-30 16:52:08 +0100

    Have different lock files for different R versions
    This allows using renv to restore the right versions of the
    packages for a specific version of R.
    
    The renv.lock file should no longer be used directly but instead
    the setup to get the right vesion

---
+++ setup_r_env.r
@@ -0,0 +1,13 @@
+version <- paste(version[c("major", "minor")], collapse = ".")
+underscored_version <- chartr(".", "_", version)
+file_name <- paste0("renv_", underscored_version, ".lock")
+if (file.exists(file_name)) {
+    if (file.exists("renv.lock")) {
+        file.remove("renv.lock")
+    }
+    file.copy(file_name, "renv.lock")
+    renv::init()
+    renv::restore()
+} else {
+    print(paste("No renv lock file for R", version))
+}
